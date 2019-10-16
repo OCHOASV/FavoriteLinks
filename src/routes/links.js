4a7edcdfb5c0968fe7aa0b4b8c7f569382562dc0
OCHOA <okochoa@hotmail.com>
2019-10-16 13:23:58 -0600

    Insertar y Listar

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,8 +1,35 @@
-/***  ***/
+/*** Links  ***/
 const express = require('express');
 const router = express.Router();
 
 // DataBase connection
-const MyPoll = require('../database');
+const MyPool = require('../database');
+
+// Ver el formulario
+router.get('/add', (req, res) => {
+		res.render('links/add');
+	}
+);
+// Se diferencean por los metodos post y get
+// Insert link
+router.post('/add', async(req, res) => {
+		// console.log(req.body);
+		const {title, url, descripcion} = req.body;
+		const newLink = {
+			title,
+			url,
+			descripcion
+		};
+		await MyPool.query('INSERT INTO links set ?', [newLink]);
+		res.redirect('/links');
+	}
+);
+
+// Listar los links
+router.get('/', async(req, res) => {
+		const links = await MyPool.query('SELECT * FROM links');
+		res.render('links/list', {links});
+	}
+);
 
 module.exports = router;
--- src/routes/router.js
+++ src/routes/router.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // Index
-router.get('/', (req, res) => {
-	res.send('It works!');
-});
+router.get('/',
+	(req, res) => {
+		res.send('It works!');
+	}
+);
+
 
 module.exports = router;
